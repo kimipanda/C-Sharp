5549ac1856e3bf158b4fc0ad5462fa3365f40658
EUIHYEN KIM <kimipanda@naver.com>
2016-04-03 18:39:17 +0900

    Simple Image Opacity&Merging
    Simple Image Opacity&Merging

---
+++ ImageOpacityMerging/ImageOpacityMerging/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace ImageOpacityMerging
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pic3 = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(380, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 161);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Result (Change) ->";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pic3
+            // 
+            this.pic3.Location = new System.Drawing.Point(503, 13);
+            this.pic3.Name = "pic3";
+            this.pic3.Size = new System.Drawing.Size(177, 161);
+            this.pic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic3.TabIndex = 2;
+            this.pic3.TabStop = false;
+            // 
+            // pic2
+            // 
+            this.pic2.Image = global::ImageOpacityMerging.Properties.Resources.test2;
+            this.pic2.Location = new System.Drawing.Point(196, 13);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(177, 161);
+            this.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic2.TabIndex = 1;
+            this.pic2.TabStop = false;
+            // 
+            // pic1
+            // 
+            this.pic1.Image = global::ImageOpacityMerging.Properties.Resources.test;
+            this.pic1.Location = new System.Drawing.Point(13, 13);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(177, 161);
+            this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic1.TabIndex = 0;
+            this.pic1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 187);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pic3);
+            this.Controls.Add(this.pic2);
+            this.Controls.Add(this.pic1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Simple Image Opacity&Merging";
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.PictureBox pic3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ImageOpacityMerging/ImageOpacityMerging/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace ImageOpacityMerging
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pic3.Image = OpacityAndMerging(pic1.Image, pic2.Image);
+        }
+
+        private Image OpacityAndMerging(Image pic1, Image pic2)
+        {
+            ColorMatrix Matrix = new ColorMatrix();
+            
+
+            Bitmap bmp = new Bitmap(pic1);
+            Rectangle rect = new Rectangle(0, 0, pic1.Width, pic1.Height);
+
+            // Opacity Setting
+            Matrix.Matrix33 = 0.5f;
+
+            using (Graphics g = Graphics.FromImage(bmp))
+            using (ImageAttributes Attributes = new ImageAttributes())
+            {
+                Attributes.SetColorMatrix(Matrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+
+                g.DrawImage(pic2, rect, 0, 0, pic2.Width, pic2.Height, GraphicsUnit.Pixel, Attributes);
+            }
+
+            return bmp;
+        }
+    }
+}
